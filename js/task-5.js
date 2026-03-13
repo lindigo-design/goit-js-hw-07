33461c70c44b9ba4916cf2e6a52164cbc527a441
Yana_Levchenko <yanalevchenko@gmail.com>
2026-03-13 09:16:11 +0100

    task-6

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,13 @@
+const button = document.querySelector(".change-color");
+const colorText = document.querySelector(".color");
+
+button.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  colorText.textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
